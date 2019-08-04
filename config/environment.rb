a75f4a22ed43f000ec56665f09561de19bc7bbd5
Mynor Xico <mynorottoniel.xico2@gmail.com>
2019-08-03 18:52:56 -0600

    Test mailing setup

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 LocalSupport::Application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.gmail.com',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :user_name      => "smtptesttesttest1@gmail.com",
+  :password       => "ngonetwork",
+  #:domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
